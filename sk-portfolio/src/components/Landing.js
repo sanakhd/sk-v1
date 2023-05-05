302bcbad07e865cdff9ff25f29e6d85aa5e63982
sana-khademi <sanakhd@my.yorku.ca>
2023-05-05 17:16:48 -0400

    Started working on the Landing page
    - added google font Inter
    - working on main section "Landing"
    - building Landing css

---
+++ sk-portfolio/src/components/Landing.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import landing from "../images/landing.png";
+
+import "../styles/Landing.css";
+
+const Landing = () => {
+  return (
+    <div id="landing">
+        <span className="title">
+            hello, I'm <br/>
+            Sana Khademi.
+        </span>
+        <span className="body">
+            designing a world I want to see
+        </span>
+    </div>
+  );
+};
+export default Landing;
